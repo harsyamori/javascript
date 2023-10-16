7a17a9ae3b409d2ba776579c214e52c3c9ba9d58
harsyamori <148061475+harsyamori@users.noreply.github.com>
2023-10-16 17:49:44 +0700

    Create ais4ds.js

---
+++ ais4ds.js
@@ -0,0 +1,8 @@
+atOptions = {
+		'key' : '5b108d93d3b37fb9bf480fb0ecd5e88c',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.bestdisplaycontent.com/5b108d93d3b37fb9bf480fb0ecd5e88c/invoke.js"></scr' + 'ipt>');
