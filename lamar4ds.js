5eb761113fbbf2719eccf8f5e8db3b5defdd74eb
harsyamori <148061475+harsyamori@users.noreply.github.com>
2023-10-16 17:43:12 +0700

    Create lamar4ds.js

---
+++ lamar4ds.js
@@ -0,0 +1,8 @@
+	atOptions = {
+		'key' : 'cbe6e0227e01808b48a0173b0c58a039',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.displaycontentprofit.com/cbe6e0227e01808b48a0173b0c58a039/invoke.js"></scr' + 'ipt>');
