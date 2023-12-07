a9bc682fd8a625851360ace1cde1b5d73c1ba67d
harsyamori <148061475+harsyamori@users.noreply.github.com>
2023-12-07 17:23:53 +0700

    Create brend4s.js

---
+++ brend4s.js
@@ -0,0 +1,8 @@
+	atOptions = {
+		'key' : '97351562ecb54b00bd7915ab980f5b08',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="//www.topcreativeformat.com/97351562ecb54b00bd7915ab980f5b08/invoke.js"></scr' + 'ipt>');
