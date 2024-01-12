dca237b38bfd132a3eef533f333c218c04e83803
harsyamori <148061475+harsyamori@users.noreply.github.com>
2024-01-12 14:47:13 +0700

    Create arto4ds.js

---
+++ arto4ds.js
@@ -0,0 +1,8 @@
+atOptions = {
+		'key' : '3ac3d561641ac134fbb448af19bee646',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="//www.topcreativeformat.com/3ac3d561641ac134fbb448af19bee646/invoke.js"></scr' + 'ipt>');
