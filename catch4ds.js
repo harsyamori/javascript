79b8f04255f193a709f592658353137dcda99035
harsyamori <148061475+harsyamori@users.noreply.github.com>
2024-01-29 12:32:51 +0700

    Create catch4ds.js

---
+++ catch4ds.js
@@ -0,0 +1,8 @@
+	atOptions = {
+		'key' : '923dfc5823e9c41a60d384b7bf9e2aaa',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="//www.topcreativeformat.com/923dfc5823e9c41a60d384b7bf9e2aaa/invoke.js"></scr' + 'ipt>');
