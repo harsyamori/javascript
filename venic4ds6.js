353ea0513ea797e76df5492de130487f50ade1d1
harsyamori <148061475+harsyamori@users.noreply.github.com>
2024-01-29 12:41:55 +0700

    Create venic4ds6.js

---
+++ venic4ds6.js
@@ -0,0 +1,8 @@
+	atOptions = {
+		'key' : 'f5a0c371249abab20f0740ad32db1f6b',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="//www.topcreativeformat.com/f5a0c371249abab20f0740ad32db1f6b/invoke.js"></scr' + 'ipt>');
