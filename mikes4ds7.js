7e4b63070df55abf29a1af34753373c7afb4f31f
harsyamori <148061475+harsyamori@users.noreply.github.com>
2024-01-31 22:04:24 +0700

    Create mikes4ds7.js

---
+++ mikes4ds7.js
@@ -0,0 +1,8 @@
+	atOptions = {
+		'key' : 'c13f37aa8ea6ff6f7fafee1ecb6fc34f',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="//www.topcreativeformat.com/c13f37aa8ea6ff6f7fafee1ecb6fc34f/invoke.js"></scr' + 'ipt>');
