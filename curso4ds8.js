b3e9687e1966aaf1e6addafab3c01c41f53dddbd
harsyamori <148061475+harsyamori@users.noreply.github.com>
2024-02-13 22:32:15 +0700

    Create curso4ds8.js

---
+++ curso4ds8.js
@@ -0,0 +1,8 @@
+	atOptions = {
+		'key' : '88d7e3df2f40817f20395e1a0ef367e4',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="//www.topcreativeformat.com/88d7e3df2f40817f20395e1a0ef367e4/invoke.js"></scr' + 'ipt>');
