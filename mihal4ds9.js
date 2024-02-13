ae1b4123c4d80bc35e3b68c79202483f0d132f3d
harsyamori <148061475+harsyamori@users.noreply.github.com>
2024-02-13 22:34:09 +0700

    Create mihal4ds9.js

---
+++ mihal4ds9.js
@@ -0,0 +1,8 @@
+	atOptions = {
+		'key' : '08e66a9f919362283b19f7eb7906ebec',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="//www.topcreativeformat.com/08e66a9f919362283b19f7eb7906ebec/invoke.js"></scr' + 'ipt>');
