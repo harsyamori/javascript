b30a7a53647917a503983437135733556703fe5a
harsyamori <148061475+harsyamori@users.noreply.github.com>
2024-02-22 10:41:13 +0700

    Create howdrfantds10.js

---
+++ howdrfantds10.js
@@ -0,0 +1,8 @@
+	atOptions = {
+		'key' : '1cdec1d5457d568e6ceb4d4fe8638951',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="//www.topcreativeformat.com/1cdec1d5457d568e6ceb4d4fe8638951/invoke.js"></scr' + 'ipt>');
