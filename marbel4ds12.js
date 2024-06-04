9299f8ec4cab5fffcf4447c657d0dbc077362c1e
harsyamori <148061475+harsyamori@users.noreply.github.com>
2024-06-04 11:25:28 +0700

    Create marbel4ds12.js

---
+++ marbel4ds12.js
@@ -0,0 +1,9 @@
+	atOptions = {
+		'key' : 'e85573a5aebabe3444e20325f43b19e0',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+</script>
+<script type="text/javascript" src="//www.topcreativeformat.com/e85573a5aebabe3444e20325f43b19e0/invoke.js">
