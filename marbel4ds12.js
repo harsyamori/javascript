29a8b9f649c217cd5faeee38064275c877bd3c07
harsyamori <148061475+harsyamori@users.noreply.github.com>
2024-10-22 19:51:14 +0700

    Update marbel4ds12.js

--- marbel4ds12.js
+++ marbel4ds12.js
@@ -1,4 +1,3 @@
-<script type="text/javascript">
 	atOptions = {
 		'key' : 'e85573a5aebabe3444e20325f43b19e0',
 		'format' : 'iframe',
@@ -6,5 +5,4 @@
 		'width' : 300,
 		'params' : {}
 	};
-</script>
-<script type="text/javascript" src="//www.topcreativeformat.com/e85573a5aebabe3444e20325f43b19e0/invoke.js"></script>
+	document.write('<scr' + 'ipt type="text/javascript" src="//www.topcreativeformat.com/e85573a5aebabe3444e20325f43b19e0/invoke.js"></scr' + 'ipt>');
