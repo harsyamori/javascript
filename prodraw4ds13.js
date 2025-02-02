1fc2b3f5830bd655d59b3d372bb6c87ab4634461
harsyamori <148061475+harsyamori@users.noreply.github.com>
2025-02-02 11:59:00 +0700

    Create prodraw4ds13.js

---
+++ prodraw4ds13.js
@@ -0,0 +1,8 @@
+	atOptions = {
+		'key' : '72b70f43457bbabd6da2099a6a01427c',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="//www.highperformanceformat.com/72b70f43457bbabd6da2099a6a01427c/invoke.js"></scr' + 'ipt>');
